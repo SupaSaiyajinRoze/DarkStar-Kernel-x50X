8e49dc77054c3a13a108583d04b26897fa6f2521
Abhishek <basevd94@gmail.com>
2017-04-25 07:16:47 +0530

    Updated Build Script

--- build.sh
+++ build.sh
@@ -44,7 +44,7 @@ export CROSS_COMPILE=/home/fester/aarch64-linux-android-4.9/bin/aarch64-linux-an
 export JOBS=16
 export SUBARCH=arm
 export KBUILD_BUILD_USER="DarkAbhi"
-export KBUILD_BUILD_HOST="FesterServer"
+export KBUILD_BUILD_HOST="Fester-Server"
 
 
 #COMPILATION SCRIPTS
