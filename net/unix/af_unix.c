0908c95df5e40c55d65f425279a017f649d23465
DarkAbhi <basevd94@gmail.com>
2017-04-25 10:26:55 +0000

    Revert "3.10.80--->3.10.81"
    This reverts commit 6fc62d7b411204534f9b3a440b64942fed99fe06.
    
    	modified:   Makefile
    	modified:   arch/mips/kernel/irq.c
    	modified:   block/genhd.c
    	modified:   drivers/ata/pata_octeon_cf.c
    	modified:   drivers/gpu/drm/i915/intel_i2c.c
    	modified:   drivers/iio/imu/adis16400.h
    	modified:   drivers/iio/imu/adis16400_core.c
    	modified:   drivers/input/mouse/elantech.c
    	modified:   drivers/net/phy/dp83640.c
    	modified:   drivers/net/phy/phy.c
    	modified:   drivers/net/xen-netback/xenbus.c
    	modified:   drivers/staging/ozwpan/ozusbsvc1.c
    	modified:   drivers/usb/serial/cp210x.c
    	modified:   drivers/usb/serial/ftdi_sio.c
    	modified:   drivers/usb/serial/ftdi_sio_ids.h
    	modified:   fs/btrfs/extent_io.c
    	modified:   fs/btrfs/super.c
    	modified:   kernel/trace/ring_buffer_benchmark.c
    	modified:   mm/memory_hotplug.c
    	modified:   net/bridge/br_multicast.c
    	modified:   net/caif/caif_socket.c
    	modified:   net/ipv4/route.c
    	modified:   net/ipv4/udp.c
    	modified:   net/ipv6/udp.c
    	modified:   net/sched/sch_api.c
    	modified:   net/unix/af_unix.c
    	modified:   net/wireless/wext-compat.c
    	modified:   sound/pci/hda/patch_realtek.c
    	modified:   sound/usb/mixer.c
    	modified:   sound/usb/mixer_maps.c

--- net/unix/af_unix.c
+++ net/unix/af_unix.c
@@ -1961,8 +1961,17 @@ static long unix_stream_data_wait(struct sock *sk, long timeo,
 	for (;;) {
 		prepare_to_wait(sk_sleep(sk), &wait, TASK_INTERRUPTIBLE);
 
-		if (sock_flag(sk, SOCK_DEAD))
-+			break;
+		if (skb_peek_tail(&sk->sk_receive_queue) != last ||
+		    sk->sk_err ||
+		    (sk->sk_shutdown & RCV_SHUTDOWN) ||
+		    signal_pending(current) ||
+		    !timeo)
+			break;
+
+		set_bit(SOCK_ASYNC_WAITDATA, &sk->sk_socket->flags);
+		unix_state_unlock(sk);
+		timeo = freezable_schedule_timeout(timeo);
+		unix_state_lock(sk);
 		clear_bit(SOCK_ASYNC_WAITDATA, &sk->sk_socket->flags);
 	}
 
