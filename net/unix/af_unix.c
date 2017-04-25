6fc62d7b411204534f9b3a440b64942fed99fe06
Abhishek <basevd94@gmail.com>
2017-04-25 14:45:17 +0530

    3.10.80--->3.10.81

--- net/unix/af_unix.c
+++ net/unix/af_unix.c
@@ -1961,17 +1961,8 @@ static long unix_stream_data_wait(struct sock *sk, long timeo,
 	for (;;) {
 		prepare_to_wait(sk_sleep(sk), &wait, TASK_INTERRUPTIBLE);
 
-		if (skb_peek_tail(&sk->sk_receive_queue) != last ||
-		    sk->sk_err ||
-		    (sk->sk_shutdown & RCV_SHUTDOWN) ||
-		    signal_pending(current) ||
-		    !timeo)
-			break;
-
-		set_bit(SOCK_ASYNC_WAITDATA, &sk->sk_socket->flags);
-		unix_state_unlock(sk);
-		timeo = freezable_schedule_timeout(timeo);
-		unix_state_lock(sk);
+		if (sock_flag(sk, SOCK_DEAD))
++			break;
 		clear_bit(SOCK_ASYNC_WAITDATA, &sk->sk_socket->flags);
 	}
 
